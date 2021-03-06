713e1ff4d696f9b169db73345b87519c78c2cece
Foontomas <popov_d@outlook.com>
2021-03-06 11:31:23 +0300

    Генерация одинаковых карточек по заданию

--- js/generation.js
+++ js/generation.js
@@ -7,57 +7,51 @@ const offerValues = {
   'bungalow': 'Бунгало',
 };
 
+const outputCards = [];
 
 //Находим шаблон #card, в шаблоне находим класс .popup
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
-
-
 
 //берём ранее сгенерированные данные
 const generateCards = dataObjects;
 
 generateCards.forEach((obj) => {
-  //клонируем карточку
-  const cardElement = cardTemplate.cloneNode(true);
+  const cardElement = cardTemplate.cloneNode(true); //клонируем карточку
+  const clonedCard = cardElement.querySelector;
+  clonedCard('.popup__title').textContent = obj.offer.title; //Вывод заголовка offer.title в .popup__title
+  clonedCard('.popup__text--address').textContent = obj.offer.address; //Вывод адреса offer.address в .popup__text--address
+  clonedCard('.popup__text--price').textContent = `${obj.offer.price} ₽/ночь`; //Вывод цены offer.price в блок .popup__text--price строкой {{offer.price}} ₽/ночь
+  clonedCard('.popup__type').textContent = offerValues[obj.offer.type]; //Вывод типа жилья в сопоставлении с подписями
+  clonedCard('.popup__text--capacity').textContent = `${obj.offer.rooms} комнат для ${obj.offer.guests} гостей`; //Вывод количества гсстей и комнат в блок .popup__text--capacity по шаблону
+  clonedCard('.popup__text--time').textContent = `Заезд после ${obj.offer.checkin}, выезд до ${obj.offer.checkout}`; //Вывод времени заезда и выезда в блок .popup__text--time по шаблону
+  clonedCard('.popup__features').innerHTML = ''; //Удаляем значения преимуществ из шаблона
 
-  cardElement.querySelector('.popup__title').textContent = obj.offer.title; //Вывод заголовка offer.title в .popup__title
-  cardElement.querySelector('.popup__text--address').textContent = obj.offer.address; //Вывод адреса offer.address в .popup__text--address
-  cardElement.querySelector('.popup__text--price').textContent = `${obj.offer.price} ₽/ночь`; //Вывод цены offer.price в блок .popup__text--price строкой {{offer.price}} ₽/ночь
-  cardElement.querySelector('.popup__type').textContent = offerValues[obj.offer.type]; //Вывод типа жилья в сопоставлении с подписями
-  cardElement.querySelector('.popup__text--capacity').textContent = `${obj.offer.rooms} комнат для ${obj.offer.guests} гостей`; //Вывод количества гсстей и комнат в блок .popup__text--capacity по шаблону
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${obj.offer.checkin}, выезд до ${obj.offer.checkout}`; //Вывод времени заезда и выезда в блок .popup__text--time по шаблону
-
+  //заполняем лист преимуществ случайными данными, если массив случайных данных не пустой
   if (obj.offer.features.length !== 0) {
-    cardElement.querySelectorAll('.popup__feature').innerHTML = '';
     obj.offer.features.forEach((item) => {
-      const element = document.createElement('p', 'popup__feature');
-      //element.classList.add(`popup__feature popup__feature--${item}`);
-      element.classList.add('popup__feature--' + item);
-      cardElement.querySelector('.popup__features').appendChild(element);
+      const element = document.createElement('li', '.popup__feature');
+      element.classList.add('popup__feature', `popup__feature--${item}`);
+      clonedCard('.popup__features').appendChild(element);
     });
   }
 
-  else {
-    cardElement.querySelector('.popup__features').classList.add('hidden');
-  }
+  clonedCard('.popup__description').textContent = obj.offer.description; //Вывод описания объекта недвижимости
 
+  //Выводим в блок .popup__photos фотографии из списка offer.photos. Каждая из строк массива photos - это src соответствующего изображения.
+  const imgTemplate = clonedCard('.popup__photo').cloneNode(true); //клонируем
+  clonedCard('.popup__photos').innerHTML = ''; //удаляем шаблонную
 
-  cardElement.querySelector('.popup__description').textContent = obj.offer.description; //Вывод описания
-
-  const imgTemplate = cardElement.querySelector('.popup__photo').cloneNode(true);
-
-  //cardElement.querySelector('.popup__photo').remove();
-
-  cardElement.querySelector('.popup__photos').innerHTML = '';
-
+  //добавляем текущую и меняем src
   obj.offer.photos.forEach((img) => {
     const newImg = imgTemplate.cloneNode();
-
     newImg.src = `${img}`;
-    cardElement.querySelector('.popup__photos').appendChild(newImg);
+    clonedCard('.popup__photos').appendChild(newImg);
   });
 
-  cardElement.querySelector('.popup__avatar').src = obj.author.avatar;
+  clonedCard('.popup__avatar').src = obj.author.avatar; //Меняем src у .popup__avatar на значения поля author.avatar объекта из массива.
 
-  document.querySelector('#map-canvas').appendChild(cardElement);
+  //document.querySelector('#map-canvas').appendChild(cardElement);
+  outputCards.push(cardElement);
 });
+
+document.querySelector('#map-canvas').appendChild(outputCards[0]);
