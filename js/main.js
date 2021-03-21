ad40f26acb779c279ba4a69d5e4e0ffb12425ea7
Foontomas <popov_d@outlook.com>
2021-03-22 02:49:57 +0300

    инактивация формы на случай отсутствия карты, сама карта и метки с балунами

--- js/main.js
+++ js/main.js
@@ -3,4 +3,7 @@ import './data.js';
 import './generation.js';
 import {formProcessing} from './form.js';
 formProcessing();
+import './map.js';
+//import {createOrdinaryMarkers} from './map.js';
+
 
